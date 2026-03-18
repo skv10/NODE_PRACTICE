6c6e4cdf7e42bf7a8bf2592afac185ee543b9f28
skv10 <sameershukla1999@gmail.com>
2026-03-18 10:48:57 +0530

    Added NodeMon and send respose as JsonResponse

--- PRACTICE_EXPRESS/index.js
+++ PRACTICE_EXPRESS/index.js
@@ -6,6 +6,10 @@ app.get('/',(req,res) => {
     res.send('Hello World!');
 });
 
+app.get('/api/v1/activity/user',(req, res) => {
+    res.status(200).json({key:"about page"});
+});
+
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!')
 }
